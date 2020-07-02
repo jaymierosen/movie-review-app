6c5f003710255c66e3f26f72345689618c96730c
Jaymie Rosen <jaymie_rosen@apple.com>
2020-07-02 17:18:02 -0400

    setting up models and api routes

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
+// Init middleware
+app.use(express.json({
+  extended: false
+}));
+
+// Define API routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+
 const PORT = process.env.PORT || 5500;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
