ab35da457cbaa53964957223352b9a38ace48864
Jaymie Rosen <jaymie_rosen@apple.com>
2020-07-08 17:06:33 -0400

    got register form to work and return a token

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,20 +5,17 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
-const normalize = require('normalize-url');
 
 const User = require('../../models/User');
 
-// @route    POST api/users
-// @desc     Register user
-// @access   Public
-// send name, email, and password to db
+// @route   POST api/users
+// @desc    Register User
+// @access  Public
 router.post(
   '/',
   [
-    // setting express validation for name, email and password
     check('name', 'Name is required').not().isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
+    check('email', 'Email is required').isEmail(),
     check(
       'password',
       'Please enter a password with 6 or more characters'
@@ -26,30 +23,29 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() }); // bad request
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { name, email, password } = req.body;
 
     try {
+      // See if user exists
       let user = await User.findOne({ email });
 
       if (user) {
-        // if user exists
         return res
           .status(400)
           .json({ errors: [{ msg: 'User already exists' }] });
       }
 
-      const avatar = normalize(
-        gravatar.url(email, {
-          s: '200', // size
-          r: 'pg', // rating
-          d: 'mm'
-        }),
-        { forceHttps: true }
-      );
+      // Get user gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
 
       user = new User({
         name,
@@ -57,33 +53,33 @@ router.post(
         avatar,
         password
       });
-      
-	  // encrypt password using bcrypt
+
+      // Encrypt password
       const salt = await bcrypt.genSalt(10);
 
+      // Hash password
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
 
       const payload = {
         user: {
-          id: user.id // mongoose uses id, not _id
+          id: user.id
         }
       };
 
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        { expiresIn: '5 days' },
+        { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
-          // return jsonwebtoken because in frontend when user registers, they can be logged in right away
           res.json({ token });
         }
       );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server Error');
+      res.status(500).send('Server error');
     }
   }
 );
--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 
-const PORT = process.env.PORT || 5600;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
