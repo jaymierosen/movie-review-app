219c330954f0756031afbc0835cf2aaa39874d43
Jaymie Rosen <jaymie_rosen@apple.com>
2020-08-21 17:16:21 -0400

    profile_error action fired only when user does not have a profile previously created...i think

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,26 +2,40 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
+import Spinner from '../layout/Spinner';
 
-const Dashboard = ({ getCurrentProfile }) => {
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
   useEffect(() => {
     getCurrentProfile()
   }, []);
-  return ( <div>Dashboard</div> )
+  // if the profile is null and still loading, show spinner
+  // else, show x, y, z
+  return loading && profile === null ?
+  <Spinner /> :
+  <Fragment>
+    <h1 className="large text-primary">
+      Dashboard
+    </h1>
+    <p>Welcome {user && user.name}!</p>
+    {profile !== null ? <Fragment>
+      <p>has profile</p>
+    </Fragment> : <Fragment>
+      <p>has no profile</p>
+    </Fragment>}
+  </Fragment>
 };
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   auth: state.auth,
   profile: state.profile
 });
 
-export default connect(
-  mapStateToProps,
-  { getCurrentProfile }
-)(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile })(
+  Dashboard
+);
